3595b80eb9c536cf002373375069b0985d0eda11
Abhishek Datta <dattaabhishek8@gmail.com>
2026-06-10 00:36:38 +0530

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: 'Abhishek@1234', // <- set your MySQL root password
+  password: 'your_password', // <- set your MySQL root password
   database: 'teacher_materials',
   waitForConnections: true,
   connectionLimit: 10,
